9463481a2e78554b791f6ce8f206706ad3d76bbd
umangkhare <umangkhare1407@gmail.com>
2019-01-21 16:06:38 +0530

    corrected

--- ARDUINO_BASIC/LED_BLINKING.ino
+++ ARDUINO_BASIC/LED_BLINKING.ino
@@ -1,4 +1,4 @@
-,// LED Blinking
+// LED Blinking
 int LED = 13;                                // connect LED at GPIO pin 13 (D7)
 
 void setup(){
@@ -9,4 +9,4 @@ void loop(){
   delay(250);                                // LED is ON for 1000 miliseconds
   digitalWrite(LED , LOW);                   // LED is OFF
   delay(250);                                // LED is OFF for 1000 miliseconds
-}s
+}
